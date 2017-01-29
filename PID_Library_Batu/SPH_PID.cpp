ba87e9a8a8936e6eaf258187306225ae15b6433f
Batu Sipka <batusipka@gmail.com>
2017-01-29 13:37:11 -0500

    Dexter PID Library v1
    need to comment..

--- PID_Library_Batu/SPH_PID.cpp
+++ PID_Library_Batu/SPH_PID.cpp
@@ -17,11 +17,7 @@ SPH_PID::SPH_PID(int motorPWM, int motorIN1,int motorIN2)
   _motorPWM = motorPWM;
 }
 
-void SPH_PID::blink(){
-  digitalWrite(_motorIN1,HIGH);
-}
-/*
-int SPH_PID::pid(int currentPosition, int targetPosition, float pConstant,int motorPWM, int motorIN1, int motorIN2, int output){
+int SPH_PID::pid(int currentPosition, int targetPosition, float pConstant, int output){
     int error = targetPosition - currentPosition;
     output = pConstant * error;
     int direction; //0 for clockwise 1 for c_clockwise
@@ -33,40 +29,43 @@ int SPH_PID::pid(int currentPosition, int targetPosition, float pConstant,int mo
 
     if(output > 0){
       direction = COUNTER_CLOCKWISE;
-      runMotor(motorPWM,motorIN1,motorIN2,output,direction);
+      runMotor(output,direction);
       return 1;
     }
     else if (output < 0 ) {
       direction = CLOCKWISE;
-      runMotor(motorPWM,motorIN1,motorIN2,-output,direction);
+      runMotor(-output,direction);
       return 2;
       }
     else{
-      runMotor(motorPWM,motorIN1,motorIN2,0,0);
+      runMotor(0,0);
       return 0;
     }
     if(abs(error) < ERROR_MARGIN){
-      runMotor(motorPWM,motorIN1,motorIN2,0,0);
+      runMotor(0,0);
       return 0;
       //initialPosition = 1;
     }
 }
 
-void SPH_PID::runMotor(int motorPWM, int motorIN1, int motorIN2, int output, int direction){
+void SPH_PID::runMotor(int output, int direction){
   if(direction == CLOCKWISE){
-    clockwise(motorIN1, motorIN2);
+    clockwise();
   }else if (direction == COUNTER_CLOCKWISE){
-    c_clockwise(motorIN1, motorIN2);
+    c_clockwise();
   }
     analogWrite(_motorPWM,output);
 }
 
-void SPH_PID::clockwise(int motorIN1, motorIN2){
+void SPH_PID::clockwise(){
   digitalWrite(_motorIN2, LOW);
   digitalWrite(_motorIN1, HIGH);
 }
 
-void SPH_PID::c_clockwise(int motorIN1, motorIN2){
+void SPH_PID::c_clockwise(){
   digitalWrite(_motorIN1, LOW);
   digitalWrite(_motorIN2, HIGH);
-}*/
+}
+void SPH_PID::setErrorMargin(int errorMargin){
+  //use to change ERROR_MARGIN in the future
+}
--- PID_Library_Batu/SPH_PID.h
+++ PID_Library_Batu/SPH_PID.h
@@ -9,16 +9,17 @@
 
 #define CLOCKWISE 0
 #define COUNTER_CLOCKWISE 1
+#define ERROR_MARGIN 10
 
 class SPH_PID
 {
 public:
   SPH_PID(int motorPWM, int motorIN1,int motorIN2);
-  void blink();
-  /*int pid(int currentPosition, int targetPosition, float pConstant,int motorPWM, int motorIN1, int motorIN2, int output);
-  void clockwise(int motorIN1, int motorIN2);
-  void c_clockwise(int motorIN1, int motorIN2);
-  void runMotor(int motorPWM, int motorIN1, int motorIN2, int output, int direction);*/
+  int pid(int currentPosition, int targetPosition, float pConstant, int output);
+  void clockwise();
+  void c_clockwise();
+  void runMotor(int output, int direction);
+  void setErrorMargin(int errorMargin);
 private:
   int _motorIN1;
   int _motorIN2;
