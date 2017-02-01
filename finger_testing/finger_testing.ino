8a9077a7b1dd1b20e04e3786cf9048de2ecf9b70
Batu Sipka <batusipka@gmail.com>
2017-02-01 15:04:20 -0500

    index and middle works at the same time!

--- finger_testing/finger_testing.ino
+++ finger_testing/finger_testing.ino
@@ -1,7 +1,6 @@
 #include <SPH_PID.h>
 
 
-
 /********************************************************
  * finger test!!
  * 
@@ -25,13 +24,17 @@ Motor_PinA - Motor_pinB - Ground - B - A
 
 /**************** VARIABLES *******************/
 #define kPIndex 4.5
-#define TARGET_POSITION 50
+#define kPMiddle 4.5
+#define TARGET_POSITION_INDEX 20
+#define TARGET_POSITION_MIDDLE 20
 #define indexPotPin 0
+#define middlePotPin 1
 
 char buffer[MAX_OUT_CHARS + 1];
 
 /*****************SPH_PID(PWM,IN1,IN2);*************************/
 SPH_PID indexPID(PWMA, AIN1, AIN2);
+SPH_PID middlePID(PWMB, BIN1, BIN2);
 
 unsigned long timePassed, timeStart, timeEnd;
 
@@ -47,17 +50,23 @@ void loop()
    
   //get current position readings   
     int currentPositionIndex = analogRead(indexPotPin);
-    //int currentPositionMiddle = analogRead(middlePotPin);
+    int currentPositionMiddle = analogRead(middlePotPin);
 
   Serial.print("Current Position old= ");
   Serial.print(currentPositionIndex);
   
    int outputIndex;
-   int targetIndex = TARGET_POSITION; 
+   int outputMiddle;
+   
+   int targetIndex = TARGET_POSITION_INDEX;
+   int targetMiddle = TARGET_POSITION_MIDDLE;
+    
    //float currentPositionIndexNew = map(currentPositionIndex,0,195,0,1000);
    //Serial.print("Current Position new= ");
    //Serial.print(currentPositionIndexNew);
     Serial.print(indexPID.pid(currentPositionIndex,targetIndex,kPIndex,outputIndex));
+    Serial.print(middlePID.pid(currentPositionMiddle,targetMiddle,kPMiddle,outputMiddle));
+
     timeEnd = millis();
     timePassed = timeEnd - timeStart;
     //Serial.print("Time = ");
