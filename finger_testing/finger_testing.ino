65ddac4fe047693ceb2a8ab0379ed43d4c1fcf68
Batu Sipka <batusipka@gmail.com>
2017-02-01 15:42:28 -0500

    thumb left, index, middle move together

--- finger_testing/finger_testing.ino
+++ finger_testing/finger_testing.ino
@@ -14,6 +14,14 @@
 #define BIN2 8
 #define PWMB 6
 
+#define AIN21 2
+#define AIN22 10
+#define PWM2A 9
+
+#define BIN21 13
+#define BIN22 12
+#define PWM2B 11
+
 #define MAX_OUT_CHARS 16
 
 /* connections
@@ -25,16 +33,24 @@ Motor_PinA - Motor_pinB - Ground - B - A
 /**************** VARIABLES *******************/
 #define kPIndex 4.5
 #define kPMiddle 4.5
-#define TARGET_POSITION_INDEX 20
-#define TARGET_POSITION_MIDDLE 20
+#define kPThumbLeft 4.5
+#define kPThumbTop 2
+#define TARGET_POSITION_INDEX 5
+#define TARGET_POSITION_MIDDLE 5
+#define TARGET_POSITION_THUMBLEFT 530
+#define TARGET_POSITION_THUMBTOP 800
 #define indexPotPin 0
 #define middlePotPin 1
+#define thumbLeftPotPin 3
+#define thumbTopPotPin 4
 
 char buffer[MAX_OUT_CHARS + 1];
 
 /*****************SPH_PID(PWM,IN1,IN2);*************************/
 SPH_PID indexPID(PWMA, AIN1, AIN2);
 SPH_PID middlePID(PWMB, BIN1, BIN2);
+SPH_PID thumbLeftPID(PWM2A, AIN21, AIN22);
+SPH_PID thumbTopPID(PWM2B, BIN21, BIN22);
 
 unsigned long timePassed, timeStart, timeEnd;
 
@@ -51,22 +67,32 @@ void loop()
   //get current position readings   
     int currentPositionIndex = analogRead(indexPotPin);
     int currentPositionMiddle = analogRead(middlePotPin);
-
-  Serial.print("Current Position old= ");
-  Serial.print(currentPositionIndex);
+    int currentPositionThumbLeft = analogRead(thumbLeftPotPin);
+    int currentPositionThumbTop = analogRead(thumbTopPotPin);
+    
+  //Serial.print("Current Position old= ");
+  //Serial.print(currentPositionIndex);
   
    int outputIndex;
    int outputMiddle;
-   
+   int outputLeft;
+   int outputTop;
+     
    int targetIndex = TARGET_POSITION_INDEX;
-   int targetMiddle = TARGET_POSITION_MIDDLE;
-    
+   int targetMiddle = TARGET_POSITION_MIDDLE;    
+   int targetThumbLeft = TARGET_POSITION_THUMBLEFT;
+   int targetThumbTop = TARGET_POSITION_THUMBTOP;
+   
    //float currentPositionIndexNew = map(currentPositionIndex,0,195,0,1000);
-   //Serial.print("Current Position new= ");
-   //Serial.print(currentPositionIndexNew);
-    Serial.print(indexPID.pid(currentPositionIndex,targetIndex,kPIndex,outputIndex));
-    Serial.print(middlePID.pid(currentPositionMiddle,targetMiddle,kPMiddle,outputMiddle));
-
+   Serial.print("Current Position Left= ");
+   Serial.print(currentPositionThumbLeft);
+   Serial.print("Current Position Top= ");
+   Serial.print(currentPositionThumbTop);
+    indexPID.pid(currentPositionIndex,targetIndex,kPIndex,outputIndex);
+    middlePID.pid(currentPositionMiddle,targetMiddle,kPMiddle,outputMiddle);
+    //ringPID.pid(currentPositionRing,targetRing,kPRing,outputRing);
+  thumbLeftPID.pid(currentPositionThumbLeft,targetThumbLeft,kPThumbLeft,outputLeft);
+  //thumbTopPID.pid(currentPositionThumbTop,targetThumbTop,kPThumbTop,outputTop);
     timeEnd = millis();
     timePassed = timeEnd - timeStart;
     //Serial.print("Time = ");
