4a73ab48bc051399930d4a547b4faa4997f69532
Batu Sipka <batusipka@gmail.com>
2017-02-07 10:18:49 -0500

    start of feb 7th

--- finger_testing/finger_testing.ino
+++ finger_testing/finger_testing.ino
@@ -6,21 +6,27 @@
  * 
  ********************************************************/
 /********************* PINS ******************/
-#define AIN1 4
-#define AIN2 5
-#define PWMA 3
+//index
+#define AIN1 22
+#define AIN2 23
+#define PWMA 2
 
-#define BIN1 7
-#define BIN2 8
-#define PWMB 6
+//middle
+#define BIN1 24
+#define BIN2 25
+#define PWMB 3
 
-#define AIN21 2
-#define AIN22 10
-#define PWM2A 9
+#define AIN21 26
+#define AIN22 27
+#define PWM2A 4
 
-#define BIN21 13
-#define BIN22 12
-#define PWM2B 11
+#define BIN21 28
+#define BIN22 29
+#define PWM2B 5
+
+#define AIN31 30
+#define AIN32 31
+#define PWM3A 6
 
 #define MAX_OUT_CHARS 16
 
@@ -33,16 +39,32 @@ Motor_PinA - Motor_pinB - Ground - B - A
 /**************** VARIABLES *******************/
 #define kPIndex 4.5
 #define kPMiddle 4.5
+#define kPRing 4.5
 #define kPThumbLeft 4.5
-#define kPThumbTop 2
-#define TARGET_POSITION_INDEX 5
-#define TARGET_POSITION_MIDDLE 5
-#define TARGET_POSITION_THUMBLEFT 530
-#define TARGET_POSITION_THUMBTOP 800
+#define kPThumbTop 4.5
+#define TARGET_POSITION_INDEX 50
+#define TARGET_POSITION_MIDDLE 150
+#define TARGET_POSITION_RING 150
+#define TARGET_POSITION_THUMBLEFT 430
+#define TARGET_POSITION_THUMBTOP 850
+
+#define INDEX_STRAIGHT 170
+#define INDEX_CURLED  5
+#define MIDDLE_STRAIGHT 170
+#define MIDDLE_CURLED 10
+#define RING_STRAIGHT 210
+#define RING_CURLED 35
+#define LEFT_LEFT 700
+#define LEFT_RIGHT  430
+#define TOP_TOP 1000
+#define TOP_MIDDLE 890
+#define TOP_DOWN  770
+
 #define indexPotPin 0
 #define middlePotPin 1
-#define thumbLeftPotPin 3
-#define thumbTopPotPin 4
+#define ringPotPin 2
+#define thumbLeftPotPin 4
+#define thumbTopPotPin 3
 
 char buffer[MAX_OUT_CHARS + 1];
 
@@ -51,6 +73,7 @@ SPH_PID indexPID(PWMA, AIN1, AIN2);
 SPH_PID middlePID(PWMB, BIN1, BIN2);
 SPH_PID thumbLeftPID(PWM2A, AIN21, AIN22);
 SPH_PID thumbTopPID(PWM2B, BIN21, BIN22);
+SPH_PID ringPID(PWM3A, AIN31, AIN32);
 
 unsigned long timePassed, timeStart, timeEnd;
 
@@ -67,32 +90,41 @@ void loop()
   //get current position readings   
     int currentPositionIndex = analogRead(indexPotPin);
     int currentPositionMiddle = analogRead(middlePotPin);
-    int currentPositionThumbLeft = analogRead(thumbLeftPotPin);
-    int currentPositionThumbTop = analogRead(thumbTopPotPin);
+    int currentPositionRing = analogRead(ringPotPin);
+    int currentPositionThumbLeft = analogRead(thumbTopPotPin);
+    int currentPositionThumbTop = analogRead(thumbLeftPotPin);
     
   //Serial.print("Current Position old= ");
   //Serial.print(currentPositionIndex);
   
    int outputIndex;
    int outputMiddle;
+   int outputRing;
    int outputLeft;
    int outputTop;
-     
-   int targetIndex = TARGET_POSITION_INDEX;
-   int targetMiddle = TARGET_POSITION_MIDDLE;    
-   int targetThumbLeft = TARGET_POSITION_THUMBLEFT;
-   int targetThumbTop = TARGET_POSITION_THUMBTOP;
+     /*
+   int targetIndex = INDEX_STRAIGHT;
+   int targetMiddle = MIDDLE_STRAIGHT; 
+   int targetRing = RING_STRAIGHT;   
+   int targetThumbLeft = LEFT_RIGHT;
+   int targetThumbTop = TOP_DOWN;
+   */
+   int targetIndex = INDEX_CURLED;
+   int targetMiddle = MIDDLE_CURLED; 
+   int targetRing = RING_CURLED;   
+   int targetThumbLeft = LEFT_LEFT;
+   int targetThumbTop = TOP_MIDDLE;
    
    //float currentPositionIndexNew = map(currentPositionIndex,0,195,0,1000);
-   Serial.print("Current Position Left= ");
-   Serial.print(currentPositionThumbLeft);
-   Serial.print("Current Position Top= ");
-   Serial.print(currentPositionThumbTop);
+   //Serial.print("Current Position INDEX= ");
+   //Serial.print(currentPositionIndex);
+   //Serial.print("Current Position Ring= ");
+   //Serial.print(currentPositionRing);
     indexPID.pid(currentPositionIndex,targetIndex,kPIndex,outputIndex);
     middlePID.pid(currentPositionMiddle,targetMiddle,kPMiddle,outputMiddle);
-    //ringPID.pid(currentPositionRing,targetRing,kPRing,outputRing);
-  thumbLeftPID.pid(currentPositionThumbLeft,targetThumbLeft,kPThumbLeft,outputLeft);
-  //thumbTopPID.pid(currentPositionThumbTop,targetThumbTop,kPThumbTop,outputTop);
+    ringPID.pid(currentPositionRing,targetRing,kPRing,outputRing);
+    thumbLeftPID.pid(currentPositionThumbLeft,targetThumbLeft,kPThumbLeft,outputLeft);
+    thumbTopPID.pid(currentPositionThumbTop,targetThumbTop,kPThumbTop,outputTop);
     timeEnd = millis();
     timePassed = timeEnd - timeStart;
     //Serial.print("Time = ");
